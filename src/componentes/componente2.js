69545b667cb52aa7d530b6c0f2a752519f56493d
Jules9004 <julytroldan@yahoo.com>
2022-10-18 20:07:02 -0500

    Iniciando proyecto

---
+++ src/componentes/componente2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Component2 extends React.Component {
+    render(){
+        return(
+            <div className='hijo2'>
+            <h1> Clase de React </h1>
+            </div>
+        )
+    }
+}
+
+export default Component2;
